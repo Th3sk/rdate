5f015f0b9c933664156f765b752d1a167d3c6751
Gabe Anzelini <me@gabeanzelini.com>
2011-12-01 11:50:07 -0800

    moved files around and added gemspec

---
+++ rdate.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'rdate'
+  s.version     = '0.0.3'
+  s.date        = '2011-12-01'
+  s.summary     = "OS X-style date command written in ruby"
+  s.description = "OS X-style date command written in ruby"
+  s.authors     = ["Gabe Anzelini"]
+  s.email       = 'me@gabeanzelini.com'
+  s.files       = ["bin/rdate"]
+  s.executables = ["rdate"]
+end
--- test/rdate_spec.rb
+++ test/rdate_spec.rb
@@ -4,19 +4,20 @@ describe 'rdate' do
   it "should output date -v1d -v+1m -v-1d -v-fri" do
 
     date_output = `date -v1d -v+1m -v-1d -v-fri`
-    rdate_output = `./rdate.rb -v1d -v+1m -v-1d -v-fri`
+    rdate_output = `bin/rdate.rb -v1d -v+1m -v-1d -v-fri`
 
     rdate_output.must_equal date_output
   end
   it "should output date -v1d -v3m -v0y -v-1d" do 
     date_output = `date -v1d -v3m -v0y -v-1d`
-    rdate_output = `./rdate.rb -v1d -v3m -v0y -v-1d`
+    rdate_output = `bin/rdate.rb -v1d -v3m -v0y -v-1d`
 
     rdate_output.must_equal date_output
   end
   it "should render date -v1m -v+1y" do
     date_output = `date -v1m -v+1y`
-    rdate_output = `./rdate.rb -v1m -v+1y`
-    fail "\nexpected #{date_output}\ngot #{rdate_output}" unless rdate_output == date_output
+    rdate_output = `bin/rdate.rb -v1m -v+1y`
+
+    rdate_output.must_equal date_output
   end
 end
